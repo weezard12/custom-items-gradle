0efb8fbe27c71c5bc44f7bf2e76b66b28664d73a
weezard12 <weezard1234@gmail.com>
2026-01-29 11:45:09 +0200

    should fix 1.19

--- kci-nms19/src/main/java/nl/knokko/customitems/nms19/KciNmsEntities19.java
+++ kci-nms19/src/main/java/nl/knokko/customitems/nms19/KciNmsEntities19.java
@@ -1,13 +1,15 @@
 package nl.knokko.customitems.nms19;
 
-import net.minecraft.server.level.WorldServer;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.damagesource.DamageSources;
 import net.minecraft.world.entity.projectile.EntityTippedArrow;
 import nl.knokko.customitems.nms16plus.KciNmsEntities16Plus;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.inventory.ItemStack;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
 class KciNmsEntities19 extends KciNmsEntities16Plus {
@@ -16,6 +18,8 @@ class KciNmsEntities19 extends KciNmsEntities16Plus {
     private static final Method DAMAGE_METHOD = findDamageMethod();
     private static volatile Method PROJECTILE_DAMAGE_METHOD;
     private static final Method SET_OWNER_METHOD = findSetOwnerMethod();
+    private static volatile Constructor<?> ARROW_CONSTRUCTOR_SIMPLE;
+    private static volatile Constructor<?> ARROW_CONSTRUCTOR_WITH_ITEM;
 
     @Override
     public void causeFakeProjectileDamage(
@@ -28,7 +32,7 @@ class KciNmsEntities19 extends KciNmsEntities16Plus {
         Object shooterHandle = getHandle(responsibleShooter);
         Object targetHandle = getHandle(toDamage);
 
-        EntityTippedArrow fakeArrow = new EntityTippedArrow((WorldServer) worldHandle,
+        EntityTippedArrow fakeArrow = createFakeArrow(worldHandle,
                 projectilePositionX, projectileMotionY, projectileMotionZ);
         setOwner(fakeArrow, shooterHandle);
         fakeArrow.projectileSource = responsibleShooter;
@@ -61,10 +65,12 @@ class KciNmsEntities19 extends KciNmsEntities16Plus {
 
     private static DamageSource createProjectileDamageSource(Object targetHandle, Object projectileHandle, Object ownerHandle) {
         try {
-            DamageSources sources = (DamageSources) DAMAGE_SOURCES_METHOD.invoke(targetHandle);
+            Object sources = DAMAGE_SOURCES_METHOD.invoke(targetHandle);
             Method method = PROJECTILE_DAMAGE_METHOD;
             if (method == null) {
-                method = findProjectileDamageMethod(sources.getClass(), projectileHandle.getClass(), ownerHandle.getClass());
+                method = findProjectileDamageMethod(
+                        sources.getClass(), projectileHandle.getClass(), ownerHandle.getClass()
+                );
                 PROJECTILE_DAMAGE_METHOD = method;
             }
             return (DamageSource) method.invoke(sources, projectileHandle, ownerHandle);
@@ -86,7 +92,15 @@ class KciNmsEntities19 extends KciNmsEntities16Plus {
     }
 
     private static Method findDamageSourcesMethod() {
-        Method method = findZeroArgMethodReturning(net.minecraft.world.entity.Entity.class, DamageSources.class);
+        Class<?> entityClass = net.minecraft.world.entity.Entity.class;
+        Class<?> damageSourcesClass = loadNmsClass("net.minecraft.world.damagesource.DamageSources");
+        Method method = null;
+        if (damageSourcesClass != null) {
+            method = findZeroArgMethodReturning(entityClass, damageSourcesClass);
+        }
+        if (method != null) return method;
+
+        method = findZeroArgMethodReturning(entityClass, "net.minecraft.world.damagesource.");
         if (method == null) {
             throw new IllegalStateException("Can't find DamageSources accessor for 1.19.x");
         }
@@ -95,7 +109,15 @@ class KciNmsEntities19 extends KciNmsEntities16Plus {
 
     private static Method findDamageMethod() {
         Method method = findDamageMethodIn(net.minecraft.world.entity.Entity.class);
-        if (method == null) method = findDamageMethodIn(net.minecraft.world.entity.EntityLiving.class);
+        if (method == null) {
+            Class<?> livingClass = loadNmsClass("net.minecraft.world.entity.EntityLiving");
+            if (livingClass == null) {
+                livingClass = loadNmsClass("net.minecraft.world.entity.LivingEntity");
+            }
+            if (livingClass != null) {
+                method = findDamageMethodIn(livingClass);
+            }
+        }
         if (method == null) {
             throw new IllegalStateException("Can't find damage method for 1.19.x");
         }
@@ -135,6 +157,39 @@ class KciNmsEntities19 extends KciNmsEntities16Plus {
             }
         }
         return null;
+    }
+
+    private static Method findZeroArgMethodReturning(Class<?> owner, String returnTypePrefix) {
+        for (Method method : owner.getMethods()) {
+            if (method.getParameterCount() != 0) continue;
+            Class<?> returnType = method.getReturnType();
+            if (returnType.getName().startsWith(returnTypePrefix) && hasDamageSourceFactory(returnType)) {
+                return method;
+            }
+        }
+        for (Method method : owner.getDeclaredMethods()) {
+            if (method.getParameterCount() != 0) continue;
+            Class<?> returnType = method.getReturnType();
+            if (returnType.getName().startsWith(returnTypePrefix) && hasDamageSourceFactory(returnType)) {
+                method.setAccessible(true);
+                return method;
+            }
+        }
+        return null;
+    }
+
+    private static boolean hasDamageSourceFactory(Class<?> owner) {
+        for (Method method : owner.getMethods()) {
+            if (method.getParameterCount() == 2 && DamageSource.class.isAssignableFrom(method.getReturnType())) {
+                return true;
+            }
+        }
+        for (Method method : owner.getDeclaredMethods()) {
+            if (method.getParameterCount() == 2 && DamageSource.class.isAssignableFrom(method.getReturnType())) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private static Method findProjectileDamageMethod(Class<?> sourcesClass, Class<?> projectileClass, Class<?> ownerClass) {
@@ -217,17 +272,22 @@ class KciNmsEntities19 extends KciNmsEntities16Plus {
     }
 
     private static Method findSetOwnerMethod() {
-        Class<?> livingClass = net.minecraft.world.entity.EntityLiving.class;
+        Class<?> entityClass = net.minecraft.world.entity.Entity.class;
+        Class<?> livingClass = loadNmsClass("net.minecraft.world.entity.EntityLiving");
+        if (livingClass == null) {
+            livingClass = loadNmsClass("net.minecraft.world.entity.LivingEntity");
+        }
+        Class<?> preferredOwnerClass = livingClass != null ? livingClass : entityClass;
         String[] candidateNames = { "b", "setOwner", "setShooter", "a" };
         for (String candidateName : candidateNames) {
             try {
-                return EntityTippedArrow.class.getMethod(candidateName, livingClass);
+                return EntityTippedArrow.class.getMethod(candidateName, preferredOwnerClass);
             } catch (NoSuchMethodException ignored) {
             }
         }
         for (String candidateName : candidateNames) {
             try {
-                Method method = EntityTippedArrow.class.getDeclaredMethod(candidateName, livingClass);
+                Method method = EntityTippedArrow.class.getDeclaredMethod(candidateName, preferredOwnerClass);
                 method.setAccessible(true);
                 return method;
             } catch (NoSuchMethodException ignored) {
@@ -237,7 +297,7 @@ class KciNmsEntities19 extends KciNmsEntities16Plus {
         for (Method method : EntityTippedArrow.class.getMethods()) {
             if (method.getParameterCount() == 1 && method.getReturnType() == void.class) {
                 Class<?> param = method.getParameterTypes()[0];
-                if (param.isAssignableFrom(livingClass)) {
+                if (param.isAssignableFrom(preferredOwnerClass)) {
                     return method;
                 }
             }
@@ -246,7 +306,7 @@ class KciNmsEntities19 extends KciNmsEntities16Plus {
         for (Method method : EntityTippedArrow.class.getMethods()) {
             if (method.getParameterCount() == 1 && method.getReturnType() == void.class) {
                 Class<?> param = method.getParameterTypes()[0];
-                if (net.minecraft.world.entity.Entity.class.isAssignableFrom(param)) {
+                if (entityClass.isAssignableFrom(param)) {
                     return method;
                 }
             }
@@ -254,8 +314,8 @@ class KciNmsEntities19 extends KciNmsEntities16Plus {
         for (Method method : EntityTippedArrow.class.getDeclaredMethods()) {
             if (method.getParameterCount() == 1 && method.getReturnType() == void.class) {
                 Class<?> param = method.getParameterTypes()[0];
-                if (param.isAssignableFrom(livingClass)
-                        || net.minecraft.world.entity.Entity.class.isAssignableFrom(param)) {
+                if (param.isAssignableFrom(preferredOwnerClass)
+                        || entityClass.isAssignableFrom(param)) {
                     method.setAccessible(true);
                     return method;
                 }
@@ -263,5 +323,92 @@ class KciNmsEntities19 extends KciNmsEntities16Plus {
         }
 
         throw new IllegalStateException("Can't find projectile owner method for 1.19.x");
+    }
+
+    private static EntityTippedArrow createFakeArrow(Object worldHandle, double x, double y, double z) {
+        Constructor<?> constructor = ARROW_CONSTRUCTOR_SIMPLE;
+        if (constructor != null) {
+            return constructArrow(constructor, worldHandle, x, y, z, null);
+        }
+        constructor = ARROW_CONSTRUCTOR_WITH_ITEM;
+        if (constructor != null) {
+            return constructArrow(constructor, worldHandle, x, y, z, createArrowStack());
+        }
+
+        Class<?> worldClass = worldHandle.getClass();
+        for (Constructor<?> candidate : EntityTippedArrow.class.getConstructors()) {
+            Class<?>[] params = candidate.getParameterTypes();
+            if (params.length == 4 && params[0].isAssignableFrom(worldClass)
+                    && params[1] == double.class && params[2] == double.class && params[3] == double.class) {
+                ARROW_CONSTRUCTOR_SIMPLE = candidate;
+                return constructArrow(candidate, worldHandle, x, y, z, null);
+            }
+            if (params.length == 5 && params[0].isAssignableFrom(worldClass)
+                    && params[1] == double.class && params[2] == double.class && params[3] == double.class
+                    && net.minecraft.world.item.ItemStack.class.isAssignableFrom(params[4])) {
+                ARROW_CONSTRUCTOR_WITH_ITEM = candidate;
+                return constructArrow(candidate, worldHandle, x, y, z, createArrowStack());
+            }
+        }
+
+        for (Constructor<?> candidate : EntityTippedArrow.class.getDeclaredConstructors()) {
+            Class<?>[] params = candidate.getParameterTypes();
+            if (params.length == 4 && params[0].isAssignableFrom(worldClass)
+                    && params[1] == double.class && params[2] == double.class && params[3] == double.class) {
+                candidate.setAccessible(true);
+                ARROW_CONSTRUCTOR_SIMPLE = candidate;
+                return constructArrow(candidate, worldHandle, x, y, z, null);
+            }
+            if (params.length == 5 && params[0].isAssignableFrom(worldClass)
+                    && params[1] == double.class && params[2] == double.class && params[3] == double.class
+                    && net.minecraft.world.item.ItemStack.class.isAssignableFrom(params[4])) {
+                candidate.setAccessible(true);
+                ARROW_CONSTRUCTOR_WITH_ITEM = candidate;
+                return constructArrow(candidate, worldHandle, x, y, z, createArrowStack());
+            }
+        }
+
+        throw new IllegalStateException("Can't find EntityTippedArrow constructor for 1.19.x");
+    }
+
+    private static EntityTippedArrow constructArrow(Constructor<?> constructor, Object worldHandle,
+                                                    double x, double y, double z,
+                                                    net.minecraft.world.item.ItemStack stack) {
+        try {
+            if (constructor.getParameterCount() == 4) {
+                return (EntityTippedArrow) constructor.newInstance(worldHandle, x, y, z);
+            }
+            return (EntityTippedArrow) constructor.newInstance(worldHandle, x, y, z, stack);
+        } catch (ReflectiveOperationException failure) {
+            throw new RuntimeException("Failed to construct fake arrow for 1.19.x", failure);
+        }
+    }
+
+    private static net.minecraft.world.item.ItemStack createArrowStack() {
+        ItemStack bukkitStack = new ItemStack(Material.ARROW, 1);
+        return asNmsCopy(bukkitStack);
+    }
+
+    private static net.minecraft.world.item.ItemStack asNmsCopy(ItemStack stack) {
+        try {
+            String packageName = Bukkit.getServer().getClass().getPackage().getName();
+            String craftVersion = packageName.substring(packageName.lastIndexOf('.') + 1);
+            Class<?> craftItemStack = Class.forName(
+                    "org.bukkit.craftbukkit." + craftVersion + ".inventory.CraftItemStack"
+            );
+            Method asNmsCopy = craftItemStack.getMethod("asNMSCopy", ItemStack.class);
+            Object nmsStack = asNmsCopy.invoke(null, stack);
+            return (net.minecraft.world.item.ItemStack) nmsStack;
+        } catch (ReflectiveOperationException | RuntimeException ex) {
+            throw new IllegalStateException("Failed to convert ItemStack to NMS for 1.19.x", ex);
+        }
+    }
+
+    private static Class<?> loadNmsClass(String name) {
+        try {
+            return Class.forName(name);
+        } catch (ClassNotFoundException ignored) {
+            return null;
+        }
     }
 }
